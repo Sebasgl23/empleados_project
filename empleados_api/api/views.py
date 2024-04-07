cdf2ab6cac18c034a599e77acae80eebf87d68cb
Sebastian Guardo <sebasgl_23@hotmail.com>
2024-04-07 13:32:07 -0500

    Add email sending when creating an empleado

--- empleados_api/api/views.py
+++ empleados_api/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from empleados_api.models import Empleado
 from empleados_api.models import Telefono
@@ -6,11 +6,26 @@ from empleados_api.models import Email
 from empleados_api.api.serializer import EmpleadoSerializer
 from empleados_api.api.serializer import TelefonoSerializer
 from empleados_api.api.serializer import EmailSerializer
+from django.core.mail import send_mail
+from rest_framework.response import Response
 
 class EmpleadoViewSet(viewsets.ModelViewSet):
   permission_classes = [IsAuthenticated]
   queryset = Empleado.objects.all()
   serializer_class = EmpleadoSerializer
+
+  def create(self, request):
+    serializer = EmpleadoSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      send_mail(
+        "Bienvenido a la empresa {} {}!".format(serializer.data['nombre'], serializer.data['apellidos']),
+        "Su registro en la plataforma de usuarios de la empresa ha sido exitoso. Bienvenido! Su cargo es {}, en el departamento de {}.".format(serializer.data['cargo'], serializer.data['departamento']),
+        None,
+        ["sebiisgl@gmail.com"]
+      )
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
 
 class TelefonoViewSet(viewsets.ModelViewSet):
   permission_classes = [IsAuthenticated]
--- empleados_project/settings.py
+++ empleados_project/settings.py
@@ -138,3 +138,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
 }
+
+DEFAULT_FROM_EMAIL = 'pruebatecnicapython@outlook.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.outlook.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = DEFAULT_FROM_EMAIL
+EMAIL_HOST_PASSWORD = 'ccmcbhinfqwnhzkw'
