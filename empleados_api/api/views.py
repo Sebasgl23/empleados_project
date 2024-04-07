608066f28be777ccb618b242c7208fdd97b31099
Sebastian Guardo <sebasgl_23@hotmail.com>
2024-04-07 12:02:20 -0500

    Add jwt auth to all the endpoints of the api

--- empleados_api/api/views.py
+++ empleados_api/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from empleados_api.models import Empleado
 from empleados_api.models import Telefono
 from empleados_api.models import Email
@@ -7,14 +8,17 @@ from empleados_api.api.serializer import TelefonoSerializer
 from empleados_api.api.serializer import EmailSerializer
 
 class EmpleadoViewSet(viewsets.ModelViewSet):
+  permission_classes = [IsAuthenticated]
   queryset = Empleado.objects.all()
   serializer_class = EmpleadoSerializer
 
 class TelefonoViewSet(viewsets.ModelViewSet):
+  permission_classes = [IsAuthenticated]
   queryset = Telefono.objects.all()
   serializer_class = TelefonoSerializer
 
 class EmailViewSet(viewsets.ModelViewSet):
+  permission_classes = [IsAuthenticated]
   queryset = Email.objects.all()
   serializer_class = EmailSerializer
 
